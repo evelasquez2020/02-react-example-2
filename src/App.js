d0581b503a55fcdaceb14e45a3eff6fe6b8e9a8d
evelasquez2020 <estebanvelasquez0852@gmail.com>
2022-11-16 16:28:37 -0500

    Primer commit

--- src/App.js
+++ src/App.js
@@ -6,9 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Practicando Subida Y Publicación a Git-Hub Pages</h1>
+        <h2>Primera Publicación</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
